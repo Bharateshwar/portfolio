ff9eac9a6e729eb45c588921a9d645a64c15485b
Bharateshwar Singh <bharateshwarsp@gmail.com>
2024-05-30 03:01:46 +0530

    chore: setup remix

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+  plugins: [
+    remix({
+      appDirectory: "src",
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
+});

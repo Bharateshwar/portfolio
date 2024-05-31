440cc75a127a0b3c316956d753e5ed65c88ad2a6
Bharateshwar Singh <bharateshwarsp@gmail.com>
2024-06-01 03:05:56 +0530

    feat: custom 404, base styling and fonts

--- src/root.tsx
+++ src/root.tsx
@@ -1,10 +1,16 @@
 import {
+  Link,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react";
+
+// Styles
+import "styles/main.scss";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -26,4 +32,19 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return <Outlet />;
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  return (
+    <>
+      <h1>
+        {isRouteErrorResponse(error) && error.status === 404
+          ? "Sorry, the thing you're looking for isn't here."
+          : "Somthing went wrong here!"}
+      </h1>
+      <Link to="/">Go home</Link>
+    </>
+  );
 }

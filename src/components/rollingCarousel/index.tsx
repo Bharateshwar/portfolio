b319bd5950bc84de266297731b4a5ae232a7a1c9
Bharateshwar Singh <bharateshwarsp@gmail.com>
2024-06-04 04:30:10 +0530

    feat: rolling animation

--- src/components/rollingCarousel/index.tsx
+++ src/components/rollingCarousel/index.tsx
@@ -1,9 +1,62 @@
+import { useEffect, useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import { MotionPathPlugin } from "gsap/dist/MotionPathPlugin";
+import gsap from "gsap/dist/gsap";
+
 import LayoutCircle from "components/svg/layoutCircle";
 import "styles/rolling-carousel.scss";
 
-const carouselItems = [{ title: "Lorem" }, { title: "ipsum" }];
+// @TODO: Move to props
+const carouselItems = [{ title: "" }, { title: "" }, { title: "" }];
 
-function RollingCaoursel() {
+function RollingCaoursel({
+  alignment = "left",
+}: {
+  alignment: "left" | "right";
+}) {
+  const mainContainerRef = useRef<HTMLDivElement>(null);
+
+  // register gsap plugins
+  useEffect(() => {
+    gsap.registerPlugin(MotionPathPlugin);
+    gsap.registerPlugin(useGSAP);
+  }, []);
+
+  useGSAP(
+    () => {
+      const itemsPlacementGap = 1 / carouselItems.length;
+      const carouselItemElements: gsap.TweenTarget[] = gsap.utils.toArray(
+        ".rolling-carousel__item"
+      );
+
+      // Reposition items to align to their centers
+      gsap.set(carouselItemElements, {
+        xPercent: -50,
+        yPercent: -50,
+        transformOrigin: "50% 50%",
+      });
+
+      // Fade items into view
+      gsap.to(carouselItemElements, { opacity: 1 });
+
+      // Animate items along svg path
+      carouselItemElements.forEach((item, index) => {
+        gsap.to(item, {
+          motionPath: {
+            path: "#layout-circle-path",
+            align: "#layout-circle-path",
+            start: index * itemsPlacementGap + (alignment === "left" ? 1 : 0),
+            end: index * itemsPlacementGap + (alignment === "left" ? 0 : 1),
+          },
+          duration: 10,
+          repeat: -1,
+          ease: "none",
+        });
+      });
+    },
+    { scope: mainContainerRef }
+  );
+
   const renderCarouselItems = () => {
     return carouselItems?.map(({ title }, index) => {
       return (
@@ -17,7 +70,7 @@ function RollingCaoursel() {
   };
 
   return (
-    <div className="rolling-carousel --left">
+    <div ref={mainContainerRef} className="rolling-carousel --left">
       <LayoutCircle />
       {renderCarouselItems()}
     </div>
--- src/components/svg/layoutCircle/index.tsx
+++ src/components/svg/layoutCircle/index.tsx
@@ -1,6 +1,7 @@
 const LayoutCircle = ({ className }: { className?: string }) => (
   <svg viewBox="0 0 700 700" className={className}>
     <path
+      id="layout-circle-path"
       d="M350 0.5a349.5 349.5 0 1 0 0 699a349.5 349.5 0 1 0 0 -699z"
       stroke="#3D3D3D"
       fill="none"

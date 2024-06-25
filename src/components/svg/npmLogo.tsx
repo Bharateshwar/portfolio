bc7545c23517a7245377e1922715279009fb1a7f
Bharateshwar Singh <bharateshwarsp@gmail.com>
2024-06-25 23:49:37 +0530

    chore: add svg logos

---
+++ src/components/svg/npmLogo.tsx
@@ -0,0 +1,19 @@
+function NpmLogo() {
+  return (
+    <svg
+      height="32"
+      width="32"
+      viewBox="0 0 700 700"
+      fill="rgb(203, 0, 0)"
+      aria-hidden="true"
+    >
+      <polygon fill="rgb(203, 0, 0)" points="0,700 700,700 700,0 0,0"></polygon>
+      <polygon
+        fill="#ffffff"
+        points="150,550 350,550 350,250 450,250 450,550 550,550 550,150 150,150 "
+      ></polygon>
+    </svg>
+  );
+}
+
+export default NpmLogo;

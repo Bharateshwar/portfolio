70923b9d8ff34bfda493ecdff733998ef81b80bd
Bharateshwar Singh <bharateshwarsp@gmail.com>
2024-06-27 04:09:51 +0530

    feat: skills section base layout

--- src/components/skills/constants.ts
+++ src/components/skills/constants.ts
@@ -18,6 +18,7 @@ import NpmLogo from 'components/svg/npmLogo';
 import ReactHookFormLogo from 'components/svg/reactHookFormLogo';
 import ReactLogo from 'components/svg/reactLogo';
 import ReduxLogo from 'components/svg/reduxLogo';
+import RemixLogo from 'components/svg/remixLogo';
 import SassLogo from 'components/svg/sassLogo';
 import StorybookLogo from 'components/svg/storybookLogo';
 import TailwindLogo from 'components/svg/tailwindLogo';
@@ -28,127 +29,176 @@ import WebflowLogo from 'components/svg/webflowLogo';
 import WebpackLogo from 'components/svg/webpackLogo';
 import YarnLogo from 'components/svg/yarnLogo';
 
-export const SKILLS_DATA = [
-  { name: 'Ant Design', logo: AntdLogo, link: 'https://ant.design' },
-  { name: 'Astro', logo: AstroLogo, link: 'https://astro.build/' },
-  { name: 'BEM', logo: BemLogo, link: 'https://getbem.com/' },
-  { name: 'Bubble', logo: BubbleLogo, link: 'https://bubble.io/' },
-  {
-    name: 'CSS',
-    logo: CssLogo,
-    link: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
-  },
-  {
-    name: 'Docker',
-    logo: DockerLogo,
-    link: 'https://www.docker.com/',
-  },
-  {
-    name: 'DotLottie',
-    logo: DotLottieLogo,
-    link: 'https://dotlottie.io/',
-  },
-  {
-    name: 'Gatsby',
-    logo: GatsbyLogo,
-    link: 'https://www.gatsbyjs.com/',
-  },
-  {
-    name: 'GraphQL',
-    logo: GraphQlLogo,
-    link: 'https://graphql.org/',
-  },
-  {
-    name: 'GSAP',
-    logo: GsapLogo,
-    link: 'https://gsap.com',
-  },
-  {
-    name: 'HTML',
-    logo: HtmlLogo,
-    link: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
-  },
-  {
-    name: 'jQuery',
-    logo: JQueryLogo,
-    link: 'https://jquery.com/',
-  },
-  {
-    name: 'JavaScript',
-    logo: JsLogo,
-    link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
-  },
-  {
-    name: 'Material UI',
-    logo: MuiLogo,
-    link: 'https://mui.com/',
-  },
-  {
-    name: 'NextJS',
-    logo: NextJsLogo,
-    link: 'https://nextjs.org/',
-  },
-  {
-    name: 'NPM',
-    logo: NpmLogo,
-    link: 'https://www.npmjs.com/',
-  },
-  {
-    name: 'React Hook Form',
-    logo: ReactHookFormLogo,
-    link: 'https://react-hook-form.com/',
-  },
-  {
-    name: 'React',
-    logo: ReactLogo,
-    link: 'https://react.dev/',
-  },
-  {
-    name: 'Redux',
-    logo: ReduxLogo,
-    link: 'https://redux.js.org/',
-  },
-  {
-    name: 'Sass',
-    logo: SassLogo,
-    link: 'https://sass-lang.com/',
-  },
-  {
-    name: 'Storybook',
-    logo: StorybookLogo,
-    link: 'https://storybook.js.org/',
-  },
-  {
-    name: 'Tailwind',
-    logo: TailwindLogo,
-    link: 'https://tailwindcss.com/',
-  },
-  {
-    name: 'TypeScript',
-    logo: TsLogo,
-    link: 'https://www.typescriptlang.org/',
-  },
-  {
-    name: 'Vue',
-    logo: VueLogo,
-    link: 'https://vuejs.org/',
-  },
-  {
-    name: 'Webflow',
-    logo: WebflowLogo,
-    link: 'https://www.webflow.com/',
-  },
-  {
-    name: 'Webpack',
-    logo: WebpackLogo,
-    link: 'https://webpack.js.org/',
-  },
+export type SkillItem = {
+  name: string;
+  logo: () => JSX.Element;
+  link: string;
+};
 
+export type SkillList = {
+  category: string;
+  items: SkillItem[];
+};
+
+type SkillDataRow = SkillList[];
+
+const ROW_ONE: SkillDataRow = [
   {
-    name: 'Yarn',
-    logo: YarnLogo,
-    link: 'https://yarnpkg.com/',
+    category: 'Frameworks & Libraries',
+    items: [
+      {
+        name: 'React',
+        logo: ReactLogo,
+        link: 'https://react.dev/',
+      },
+      {
+        name: 'Storybook',
+        logo: StorybookLogo,
+        link: 'https://storybook.js.org/',
+      },
+      {
+        name: 'Tailwind',
+        logo: TailwindLogo,
+        link: 'https://tailwindcss.com/',
+      },
+      {
+        name: 'TypeScript',
+        logo: TsLogo,
+        link: 'https://www.typescriptlang.org/',
+      },
+      {
+        name: 'Vue',
+        logo: VueLogo,
+        link: 'https://vuejs.org/',
+      },
+      {
+        name: 'Webflow',
+        logo: WebflowLogo,
+        link: 'https://www.webflow.com/',
+      },
+      {
+        name: 'Webpack',
+        logo: WebpackLogo,
+        link: 'https://webpack.js.org/',
+      },
+    ],
   },
-  { name: 'Lodash', logo: LodashLogo, link: 'https://lodash.com/' },
-  { name: 'Vite', logo: ViteLogo, link: 'https://vitejs.dev/' },
+];
+
+const ROW_TWO: SkillDataRow = [
+  {
+    category: 'Lorem ipssum dolor sit amet',
+    items: [
+      {
+        name: 'GSAP',
+        logo: GsapLogo,
+        link: 'https://gsap.com',
+      },
+      {
+        name: 'HTML',
+        logo: HtmlLogo,
+        link: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
+      },
+      {
+        name: 'jQuery',
+        logo: JQueryLogo,
+        link: 'https://jquery.com/',
+      },
+      {
+        name: 'JavaScript',
+        logo: JsLogo,
+        link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+      },
+      {
+        name: 'Material UI',
+        logo: MuiLogo,
+        link: 'https://mui.com/',
+      },
+      {
+        name: 'NextJS',
+        logo: NextJsLogo,
+        link: 'https://nextjs.org/',
+      },
+      {
+        name: 'NPM',
+        logo: NpmLogo,
+        link: 'https://www.npmjs.com/',
+      },
+      {
+        name: 'React Hook Form',
+        logo: ReactHookFormLogo,
+        link: 'https://react-hook-form.com/',
+      },
+      {
+        name: 'Redux',
+        logo: ReduxLogo,
+        link: 'https://redux.js.org/',
+      },
+      {
+        name: 'Sass',
+        logo: SassLogo,
+        link: 'https://sass-lang.com/',
+      },
+    ],
+  },
+];
+
+const ROW_THREE: SkillDataRow = [
+  {
+    category: 'Lorem ipsum dolor sit amet',
+    items: [
+      {
+        name: 'Docker',
+        logo: DockerLogo,
+        link: 'https://www.docker.com/',
+      },
+      {
+        name: 'DotLottie',
+        logo: DotLottieLogo,
+        link: 'https://dotlottie.io/',
+      },
+      {
+        name: 'Gatsby',
+        logo: GatsbyLogo,
+        link: 'https://www.gatsbyjs.com/',
+      },
+      {
+        name: 'GraphQL',
+        logo: GraphQlLogo,
+        link: 'https://graphql.org/',
+      },
+      { name: 'Remix', logo: RemixLogo, link: 'https://remix.run/' },
+    ],
+  },
+];
+
+const ROW_FOUR: SkillDataRow = [
+  {
+    category: 'Lorem ipsum',
+    items: [
+      { name: 'Ant Design', logo: AntdLogo, link: 'https://ant.design' },
+      { name: 'Astro', logo: AstroLogo, link: 'https://astro.build/' },
+      { name: 'BEM', logo: BemLogo, link: 'https://getbem.com/' },
+      { name: 'Bubble', logo: BubbleLogo, link: 'https://bubble.io/' },
+      {
+        name: 'CSS',
+        logo: CssLogo,
+        link: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+      },
+      {
+        name: 'Yarn',
+        logo: YarnLogo,
+        link: 'https://yarnpkg.com/',
+      },
+      { name: 'Lodash', logo: LodashLogo, link: 'https://lodash.com/' },
+      { name: 'Vite', logo: ViteLogo, link: 'https://vitejs.dev/' },
+    ],
+  },
+];
+
+export const SKILL_DATA_ROWS: SkillDataRow[] = [
+  ROW_ONE,
+  ROW_TWO,
+  ROW_THREE,
+  ROW_FOUR,
 ];
--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -1,14 +1,44 @@
+import { useGSAP } from '@gsap/react';
+import { useRef } from 'react';
+
 import 'styles/skills.scss';
-import { SKILLS_DATA } from './constants';
+import { SKILL_DATA_ROWS, SkillItem, SkillList } from './constants';
 
 function Skills() {
-  return (
-    <div className="skills-section">
-      {SKILLS_DATA.map(({ name, logo: Logo }) => (
-        <div className="skill-item" key={name}>
-          <div className="skill-item__logo">
+  const mainContainerRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      // @TODO: Add animation logic
+    },
+    { scope: mainContainerRef },
+  );
+
+  const renderSkillItems = (items: SkillItem[]) =>
+    items.map(({ name, link, logo: Logo }) => (
+      <li className="skills-list__item" key={name}>
+        <a href={link} target="_blank" rel="noreferrer">
+          <div className="skills-list__item__logo">
             <Logo />
           </div>
+        </a>
+      </li>
+    ));
+
+  const renderSkillList = ({ category, items }: SkillList) => {
+    return (
+      <div className="skills-list" key={category}>
+        <p className="skills-list__title">{category}</p>
+        <ul>{renderSkillItems(items)}</ul>
+      </div>
+    );
+  };
+
+  return (
+    <div className="skills-section" ref={mainContainerRef}>
+      {SKILL_DATA_ROWS.map((dataRow, index) => (
+        <div className="skills-row" key={index}>
+          {dataRow.map(renderSkillList)}
         </div>
       ))}
     </div>

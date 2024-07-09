c0eaf25fa23e0ee099d5ded0b16205369e461cd2
Bharateshwar Singh <bharateshwarsp@gmail.com>
2024-07-10 01:02:38 +0530

    feat: container variants

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,13 +1,23 @@
 import cx from 'classnames';
 import { ReactNode } from 'react';
 
-function Container({ children, className }: Props) {
-  return <div className={cx('container', className)}>{children}</div>;
+function Container({ children, className, variant }: Props) {
+  return (
+    <div
+      className={cx('container', className, {
+        'container--gradient': variant === 'gradient',
+        'container--secondary': variant === 'secondary',
+      })}
+    >
+      {children}
+    </div>
+  );
 }
 
 interface Props {
   children: ReactNode;
   className?: string;
+  variant?: 'gradient' | 'secondary';
 }
 
 export default Container;

47b67fb11b20322422b7cf920506f50e1ee81e52
Bharateshwar Singh <bharateshwarsp@gmail.com>
2024-07-14 01:12:25 +0530

    feat: fullWidth container

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -2,7 +2,7 @@ import cx from 'classnames';
 import { ForwardedRef, forwardRef, ReactNode } from 'react';
 
 function Container(
-  { children, className, variant, isSection }: Props,
+  { children, className, variant, isSection, fullWidth }: Props,
   ref: ForwardedRef<HTMLDivElement>,
 ) {
   const WrapperElement = isSection ? 'section' : 'div';
@@ -12,6 +12,7 @@ function Container(
       className={cx('container', className, {
         'container--gradient': variant === 'gradient',
         'container--secondary': variant === 'secondary',
+        'container--full-width': fullWidth,
       })}
       ref={ref}
     >
@@ -25,6 +26,7 @@ interface Props {
   className?: string;
   variant?: 'gradient' | 'secondary';
   isSection?: boolean;
+  fullWidth?: boolean;
 }
 
 export default forwardRef(Container);

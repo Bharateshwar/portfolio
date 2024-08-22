0defc80eb64193f51dd358f982bcd0dd1af5ba79
Bharateshwar Singh <bharateshwarsp@gmail.com>
2024-08-22 21:18:21 +0530

    feat: testimonials autoscroll

--- src/components/testimonials/index.tsx
+++ src/components/testimonials/index.tsx
@@ -1,16 +1,57 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap/dist/gsap';
+import ScrollToPlugin from 'gsap/dist/ScrollToPlugin';
+import { useRef } from 'react';
+
 import 'styles/testimonials.scss';
 import { TESTIMONIALS } from './constants';
 
 function Testimonials() {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const scrollAnimationRef = useRef<GSAPAnimation | null>(null);
+
+  useGSAP(
+    () => {
+      gsap.registerPlugin(ScrollToPlugin);
+
+      scrollAnimationRef.current = gsap.to(containerRef.current, {
+        scrollTo: {
+          x: 'max',
+          autoKill: true,
+          // @TODO: Explore adding scrolling after autokill
+          onAutoKill() {
+            scrollAnimationRef.current = null;
+          },
+        },
+        ease: 'none',
+        duration: TESTIMONIALS.length * 4.5,
+        yoyo: true,
+        repeat: -1,
+      });
+    },
+    { scope: containerRef },
+  );
+
+  const pauseScrollAnimation = () => scrollAnimationRef.current?.pause();
+
+  const playScrollAnimation = () => scrollAnimationRef.current?.play();
+
   return (
-    <section className="testimonials-section hide-scrollbar">
+    <section className="testimonials-section hide-scrollbar" ref={containerRef}>
       {TESTIMONIALS.map(
         (
           { name, company, designation, image, pretext, excerpt, posttext },
           index,
         ) => {
           return (
-            <div className="testimonial" key={index}>
+            <div
+              className="testimonial"
+              key={index}
+              onMouseOver={pauseScrollAnimation}
+              onFocus={pauseScrollAnimation}
+              onMouseOut={playScrollAnimation}
+              onBlur={playScrollAnimation}
+            >
               <div className="testimonial__intro">
                 <img
                   src={image}

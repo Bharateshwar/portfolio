bdeaa766f5628fa8fc64026166f35ffc5061941d
Bharateshwar Singh <bharateshwarsp@gmail.com>
2024-08-23 00:19:22 +0530

    feat: resume button scroll triggered transition

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,6 +1,32 @@
+import { useGSAP } from '@gsap/react';
 import { Link } from '@remix-run/react';
+import gsap from 'gsap/dist/gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+
+import { SLIDES_ITEM_CONTENT_SELECTOR } from 'components/rollingCarousel/constants';
 
 function Nav() {
+  useGSAP(() => {
+    const targets: HTMLAnchorElement[] = gsap.utils.toArray(
+      '.nav__link--resume, .nav__link--hire, .nav__bottom',
+    );
+
+    const rollingCarouselFirstSlide = document.querySelector(
+      SLIDES_ITEM_CONTENT_SELECTOR,
+    ) as HTMLDivElement;
+
+    ScrollTrigger.create({
+      trigger: rollingCarouselFirstSlide.parentNode as HTMLDivElement,
+      start: 'top 10%',
+      onEnter() {
+        targets.forEach((target) => target.classList.add('--dark'));
+      },
+      onLeaveBack() {
+        targets.forEach((target) => target.classList.remove('--dark'));
+      },
+    });
+  });
+
   return (
     <nav className="nav">
       <div className="nav__top">

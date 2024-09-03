0617ee9b17a67978e80699974124a8bd0ea3be3e
Bharateshwar Singh <bharateshwarsp@gmail.com>
2024-09-03 23:26:37 +0530

    fix: remove unused nav links

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -8,7 +8,7 @@ import { SLIDES_ITEM_CONTENT_SELECTOR } from 'components/rollingCarousel/constan
 function Nav() {
   useGSAP(() => {
     const targets: HTMLAnchorElement[] = gsap.utils.toArray(
-      '.nav__link--resume, .nav__link--hire, .nav__bottom',
+      '.nav, .nav__link--resume, .nav__link--hire',
     );
 
     const rollingCarouselFirstSlide = document.querySelector(
@@ -28,28 +28,18 @@ function Nav() {
   });
 
   return (
-    <nav className="nav">
-      <div className="nav__top">
-        <Link to="/about" className="nav__link">
-          About me
-        </Link>
-        <Link to="/skills" className="nav__link">
-          Skills
-        </Link>
-      </div>
-      <div className="nav__bottom">
-        <a
-          href="/bharateshwar's resume.pdf"
-          className="nav__link nav__link--resume"
-          download
-        >
-          Resume
-        </a>
-        <Link to="/contact" className="nav__link nav__link--hire">
-          Hire me
-        </Link>
-      </div>
-    </nav>
+    <div className="nav">
+      <a
+        href="/bharateshwar's resume.pdf"
+        className="nav__link nav__link--resume"
+        download
+      >
+        Resume
+      </a>
+      <Link to="/contact" className="nav__link nav__link--hire">
+        Hire me
+      </Link>
+    </div>
   );
 }
 

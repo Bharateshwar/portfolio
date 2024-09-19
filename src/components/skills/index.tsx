e3055fb622c2af338304469f00211a7178aa2e9f
Bharateshwar Singh <bharateshwarsp@gmail.com>
2024-09-20 00:09:56 +0530

    fix: skills row content

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -3,7 +3,12 @@ import gsap from 'gsap/dist/gsap';
 import { useRef } from 'react';
 
 import 'styles/skills.scss';
-import { SKILL_DATA_ROWS, SkillDataRow, SkillItem } from './constants';
+import {
+  ANIMATION_DURATION,
+  SKILL_DATA_ROWS,
+  SkillDataRow,
+  SkillItem,
+} from './constants';
 
 function Skills() {
   const mainContainerRef = useRef<HTMLDivElement>(null);
@@ -22,7 +27,7 @@ function Skills() {
           gsap.to(row, {
             x: (index % 2 === 0 ? -1 : 1) * widthToScroll,
             ease: 'none',
-            duration: 10,
+            duration: ANIMATION_DURATION,
             yoyo: true,
             repeat: -1,
           });
